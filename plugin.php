ea132a13cfab0f57f3ac2a8f4d086dca1a294ee3
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-27 21:52:43 +0200

    SEARCH-FORM menu item handling

--- plugin.php
+++ plugin.php
@@ -8,3 +8,14 @@
  */
 
 // 🕸 \\
+
+namespace FDD\Core;
+
+function menu_search_form($title, $item, $args, $depth) {
+  if (preg_match('/SEARCH-FORM/', $title)) {
+    $title = get_search_form(false);
+  }
+  return $title;
+}
+
+add_filter('walker_nav_menu_start_el', 'FDD\Core\menu_search_form', 10, 4);
