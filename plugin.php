bc04db63a25bf4e0bf8d62f9846b0a20a60adaa7
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-07-14 15:37:09 +0200

    search only for posts

--- plugin.php
+++ plugin.php
@@ -10,5 +10,11 @@
 namespace FDD\Core;
 
 add_filter('option_use_smilies', '__return_false');
+add_filter('pre_get_posts', function($query) {
+  if ($query->is_search) {
+    $query->set('post_type', 'post');
+  }
+  return $query;
+});
 
 require_once 'et_import/converter.php';
