342750117e6831f249b094ca5a5c9de7dcbb46d5
Honza Bambas :mayhemer <honzab@firemni.cz>
2020-10-27 19:21:20 +0100

    live import, set_time_limit to 30 minutes

--- live_import/converter.php
+++ live_import/converter.php
@@ -3,7 +3,7 @@
 require_once 'import_filters.php';
 
 function fdd_import_start() {
-  set_time_limit(6000);
+  set_time_limit(30 * 60); // arg is 'seconds'
   fdd_reg_filters();
 }
 add_action('import_start', 'fdd_import_start');
